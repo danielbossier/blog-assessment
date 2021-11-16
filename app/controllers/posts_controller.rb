638c7a581bf01da03405552b1a5364c37f67ff25
danielbossier <dbossier21@gmail.com>
2021-11-15 16:09:47 -0800

    updating routes and controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,17 @@ class PostsController < ApplicationController
   require "rest-client"
 
   def get_posts
-    url = "https://api.hatchways.io/assessment/blog/posts?tag=history"
+    url = "https://api.hatchways.io/assessment/blog/posts?tag=tech"
     response = RestClient.get(url)
+    render json: response
+
+    tag = params["tag"]
+  end
+
+  def tag_posts
+    url = "https://api.hatchways.io/assessment/blog/posts?tag=#{params}"
+    response = RestClient.get(url)
+    tag = params["tag"]
     render json: response
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/posts" => "posts#get_posts"
+  get "/posts/:tag" => "posts#tag_posts"
   resources :posts
 end
